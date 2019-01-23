fa870c37f80e766f8d72a1a4de715a79e4f4af87
Tunti1 <45206192+Tunti1@users.noreply.github.com>
2019-01-23 15:08:06 +0100

    Add files via upload

---
+++ Nadruzbenica.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace Projekt
+{
+    public partial class Narudzbenica : Form
+    {
+
+        private const string ConnectionString = @"Data Source=DESKTOP-71KF32U;Initial Catalog=Baza;Integrated Security=True;Pooling=False";
+        SqlConnection con = new SqlConnection(ConnectionString);
+        public Narudzbenica()
+        {
+            InitializeComponent();
+            popuni_combo();
+        }
+
+       
+
+        private void Izlaz(object sender, EventArgs e)
+        {
+            this.Hide();
+            Servis otvori = new Servis();
+            otvori.Show();
+        }
+
+        private void Stvori_novog(object sender, EventArgs e)
+        {
+            this.Hide();
+            Kooperant otvori = new Kooperant();
+            otvori.Show();
+        }
+
+        void popuni_combo()
+        {
+            con.Open();
+            string izvadi = "select * from  Kooperant;";
+            SqlCommand izvadi_cmd = new SqlCommand(izvadi, con);
+            SqlDataReader citac;
+            citac = izvadi_cmd.ExecuteReader();
+
+            
+            while (citac.Read())
+            {
+
+                string Snaziv = citac.GetString(1);
+
+                KomboKop.Items.Add(Snaziv);
+            }
+            con.Close();
+        }
+
+        private void Spremi_narudzbenicu(object sender, EventArgs e)
+        {
+            con.Open();
+            string zadatak = "INSERT INTO Narudzbenica (Broj, Djelovi,Kooperant)  VALUES( " + this.Broj.Text + " ,'" + this.Djelovi.Text + "','" + this.KomboKop.Text.ToString() + "'); ";
+            SqlCommand cmdnar = new SqlCommand(zadatak, con);
+
+            cmdnar.ExecuteNonQuery();
+
+            Broj.Text = "";
+            Djelovi.Text = "";
+            KomboKop.Text = "";
+
+            con.Close();
+
+        }
+    }
+        
+        
+        
+            
+
+    
+                
+
+
+
+
+        
+    
+}
---
+++ Servis.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Projekt
+{
+    public partial class Servis : Form
+    {
+
+        public int sifra;
+        public string marka;
+        public string model;
+        public string komponenta;
+        public string rok;
+        public int cijena;
+        public int vrijeme;
+
+        List<Servis> servisi = new List<Servis>();
+
+
+        private const string ConnectionString = @"Data Source=DESKTOP-71KF32U;Initial Catalog=Baza;Integrated Security=True;Pooling=False";
+        SqlConnection con = new SqlConnection(ConnectionString);
+        public Servis()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void Izadji(object sender, EventArgs e)
+        {
+            this.Hide();
+            Ulaz otvori = new Ulaz();
+            otvori.Show();
+        }
+
+        private void Otvori_Narudzbenicu(object sender, EventArgs e)
+        {
+            this.Hide();
+            Narudzbenica otvori = new Narudzbenica();
+            otvori.Show();
+        }
+
+         void SpremiUredjaj(object sender, EventArgs e)
+        {
+
+
+
+
+            con.Open();
+
+             
+            string zadatak = "INSERT INTO Servis(Sifra,Marka,Model,Komponenta,Rok,Cijena,Potrebno_vrijeme)  VALUES("+this.Sifratxt.Text+" , ' "+this.MarkaTxt.Text+"' , '"+this.ModelTxT.Text+"', '"+this.KomponentaTxt.Text+ "' ,'"+this.RokKraj.Text+"' , "+this.CijenaTxtic.Text+" , "+this.Vrijeme.Text+" );";
+            SqlCommand cmd = new SqlCommand (zadatak, con);
+            
+            cmd.ExecuteNonQuery();
+            
+            con.Close();
+
+            this.Sifratxt.Text = " ";
+            this.MarkaTxt.Text = " ";
+            this.ModelTxT.Text = " ";
+            this.KomponentaTxt.Text = " ";
+            this.CijenaTxtic.Text = " ";
+            this.Vrijeme.Text = " ";
+
+
+
+        }
+
+        
+
+       
+    }
+}
